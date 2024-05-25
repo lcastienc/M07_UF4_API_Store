803af421a3ba58c5d06b28d89055afcd83bbecd9
lcastienc <2023_luis.castillo@iticbcn.cat>
2024-05-25 21:15:08 +0200

    Afegir nous productes

--- Api_Store/cataleg/views.py
+++ Api_Store/cataleg/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializer import ProductSerializer
+from .models import Product
+
 
 # Create your views here.
+
+#Afegir producte
+@api_view(['POST'])
+def add_product(request):
+    serializer = ProductSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors)
