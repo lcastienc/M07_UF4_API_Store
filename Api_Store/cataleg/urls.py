fd3b1ddb58ada0ec7bac8e0f2810cc817b24e704
lcastienc <2023_luis.castillo@iticbcn.cat>
2024-05-25 21:20:06 +0200

    Actualitzar productes

--- Api_Store/cataleg/urls.py
+++ Api_Store/cataleg/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('add_product/', views.add_product, name='add_product'),
+    path('products/add_product/', views.add_product, name='add_product'),
+    path('products/update_product/<str:pk>/', views.update_product, name='update_product')
 ]
--- Api_Store/cataleg/views.py
+++ Api_Store/cataleg/views.py
@@ -15,3 +15,17 @@ def add_product(request):
         serializer.save()
         return Response(serializer.data)
     return Response(serializer.errors)
+
+#Actulizar un producto
+@api_view(['PUT'])
+def update_product(request, pk):
+    try:
+        product = Product.objects.get(pk=pk)
+    except Product.DoesNotExist:
+        return Response({"error": "El producto que quieres modificar no existe"}, status=404)
+    serializer = ProductSerializer(product, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response({"message": 'Producto actualizado exitosamente', "data": serializer.data}, status=200)
+    return Response({"error": serializer.errors, "message": 'No se ha podido actualizar el producto'}, status=400)
+
