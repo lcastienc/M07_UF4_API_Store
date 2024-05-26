6e04c86352679306b593bbcf8f1ee1bca6f19865
lcastienc <2023_luis.castillo@iticbcn.cat>
2024-05-26 22:30:06 +0200

    Afegir productes al carretó

--- Api_Store/carreto/views.py
+++ Api_Store/carreto/views.py
@@ -22,3 +22,55 @@ def create_carreto(request):
     carreto = Carreto.objects.create(client_id=client_id)
     serializer = CarretoSerializer(carreto)
     return Response({"status": "success", "message": "Carrito creado exitosamente", "data": serializer.data}, status=201)
+
+#Afegir productes al carretó
+@api_view(['POST'])
+def add_product_to_carreto(request):
+    carreto_id = request.data.get('carreto_id')
+    product_id = request.data.get('product_id')
+    quantity = request.data.get('quantity', 1)
+
+    try:
+        carreto = Carreto.objects.get(id=carreto_id, finalitzat=False)
+    except Carreto.DoesNotExist:
+        return Response({"status": "error", "message": "No hay carrito abierto para este cliente"}, status=404)
+
+    try:
+        product = Product.objects.get(id=product_id)
+    except Product.DoesNotExist:
+        return Response({"status": "error", "message": "Producto no encontrado"}, status=404)
+
+    if product.stock < quantity:
+        return Response({"status": "error", "message": "No hay suficiente stock disponible"}, status=400)
+
+    # Calcular el precio total por la cantidad de productos
+    total_price = product.price * quantity
+
+    # Crear la relación CarretoProduct
+    carreto_product = CarretoProduct.objects.create(carreto=carreto, product=product, quantitat=quantity, preu=total_price)
+
+    # Actualizar el precio total del carrito
+    carreto.preu_total += total_price
+    carreto.save()
+
+    # Construir la respuesta completa
+    response_data = {
+        "status": "success",
+        "message": "Producto agregado al carrito exitosamente",
+        "carreto_info": {
+            "id": carreto.id,
+            "client": carreto.client.id,
+            "preu_total": carreto.preu_total,
+            "finalitzat": carreto.finalitzat,
+            "created_at": carreto.created_at,
+            "updated_at": carreto.updated_at,
+        },
+        "product_info": {
+            "product_id": product.id,
+            "name": product.name,
+            "price": product.price,
+            "quantity": quantity,
+            "total_price_for_this_product": total_price
+        }
+    }
+    return Response(response_data, status=201)
